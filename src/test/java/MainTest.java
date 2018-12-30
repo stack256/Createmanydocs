397bf30153f8c2f7f726e669ce6e6119cc5f39e5
Stas <stas.yakubov@gmail.com>
2018-12-30 16:50:59 +0500

    start

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void Test1() {
+
+        System.out.println("launching chrome browser");
+        String driverPath = "C:\\Webdrivers\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", driverPath);
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "http://demo.guru99.com/test/newtours/";
+        driver.get(baseUrl);
+        String expectedTitle = "Welcome: Mercury Tours";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+        driver.close();
+    }
+}
