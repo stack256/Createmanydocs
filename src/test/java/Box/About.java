a18b90550d973e4507ca5d62fdcab11998c7b754
syakubov <syakubov@blogic.ru>
2019-01-10 15:14:04 +0500

    Обновить 'src/test/java/Box/About.java'

--- src/test/java/Box/About.java
+++ src/test/java/Box/About.java
@@ -50,9 +50,10 @@ class About {
         Properties properties = new Properties();
         properties.put("mail.debug"          , "false"  );
         properties.put("mail.store.protocol" , "imaps"  );
-        properties.put("mail.imap.ssl.enable", "false"   );
+        properties.put("mail.imap.ssl.enable", "true"   );
         properties.put("mail.imap.port"      , IMAP_Port);
-      	properties.put("mail.imaps.ssl.trust", "*");
+      	// SSL Factory 
+        properties.put("mail.imap.socketFactory.class", "javax.net.ssl.SSLSocketFactory");   
 
         Authenticator auth = new EmailAuthenticator(IMAP_AUTH_EMAIL, IMAP_AUTH_PWD);
         Session session = Session.getDefaultInstance(properties, auth);
